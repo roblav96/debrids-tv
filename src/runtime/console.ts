f1885db698b6ab529dff6e8fe0732dae960b0e54
Robert Laverty <roblav96@gmail.com>
2021-01-02 22:39:19 -0500

    added console clear on app reload via HMR

--- src/runtime/console.ts
+++ src/runtime/console.ts
@@ -1,9 +1,11 @@
-import * as Application from '@nativescript/core/application'
-import * as R from 'rambdax'
-
 import * as dayjs from 'dayjs'
+import * as R from 'rambdax'
 import * as relativeTime from 'dayjs/plugin/relativeTime'
+
 dayjs.extend(relativeTime)
+
+console.log('\x1b[2J\x1b[3J\x1b[1;1H')
+console.log(`████  ${dayjs().format('hh:mm:ss A')}  ████`)
 
 console.log = new Proxy(console.log, {
 	apply(method, ctx: Console, args: any[]) {
@@ -23,4 +25,12 @@ console.error = new Proxy(console.error, {
 		args.unshift('\n✘')
 		return Reflect.apply(method, ctx, args)
 	},
+})
+
+import * as Application from '@nativescript/core/application'
+Application.on('uncaughtError', function uncaughtError(args) {
+	console.error('████  UNCAUGHT ERROR  ████ ->', args.error)
+})
+Application.on('discardedError', function discardedError(args) {
+	console.error('████  DISCARDED ERROR  ████ ->', args.error)
 })
